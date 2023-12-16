c38eba86d4df87ff90c82c8e2310e2eedde255e9
Son Goku <vaibhavgarg541@gmail.com>
2023-12-16 17:12:38 +0530

    utils done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv";
+import express from "express";
 import connectDB from "./db/connectDB.js";
+
+const app = express();
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 4001, () => {
+      console.log(`Server is serving at http://localhost:${process.env.PORT}/`);
+    });
+  })
+  .catch((err) => {
+    console.log("DB connection Failed !!!", err);
+  });
